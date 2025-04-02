fe37acafcd9092586b534a5dfb3d1ba460238cfc
Leonardo Lyra <lyraleo@gmail.com>
2025-04-02 18:02:28 -0300

    feat: adiciona novas operações cadastrar_usuario e cadastrar_conta e adapta o código para receber diversos usuários e salvar as contas, usuários e operações em um csv

--- main.py
+++ main.py
@@ -1,32 +1,109 @@
-from operacoes import deposito, saque, mostrar_extrato
-
-menu = '''
-
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
-
-=> '''
-
-saldo = 0
-limite = 500
-extrato = ''
-numero_saques = 0
-LIMITE_SAQUES = 3
-
-while True:
-    opcao = input(menu)
-
-    if opcao == 'd':
-        saldo, extrato = deposito(saldo, extrato)
-    elif opcao == 's':
-        saldo, extrato, numero_saques = saque(saldo, extrato, numero_saques, limite, LIMITE_SAQUES)
-    elif opcao == 'e':
-        mostrar_extrato(extrato, saldo)
-    elif opcao == 'q':
-        break
-    else:
-        print('Operação inválida, por favor selevione novamente a operação desejada.')
+import csv
+from operacoes import deposito, saque, mostrar_extrato, cadastrar_usuario, cadastrar_conta
 
 
+def show_menu_cc(cc):
+    menu = '''
+
+    [d] Depositar
+    [s] Sacar
+    [e] Extrato
+    [cc] Cadastrar conta
+    [q] Sair
+
+    => '''
+
+    saldo = cc['saldo']
+    limite = 500
+    extrato = cc['extrato']
+    numero_saques = 0
+    LIMITE_SAQUES = 3
+
+    while True:
+        opcao = input(menu)
+
+        if opcao == 'd':
+            saldo, extrato = deposito(saldo, extrato)
+            atualizar_conta(cc['numero_conta'], cc['agencia'], cc['usuario'], saldo, extrato)
+        elif opcao == 's':
+            saldo, extrato, numero_saques = saque(saldo, extrato, numero_saques, limite, LIMITE_SAQUES)
+            atualizar_conta(cc['numero_conta'], cc['agencia'], cc['usuario'], saldo, extrato)
+        elif opcao == 'e':
+            mostrar_extrato(extrato, saldo)
+        elif opcao == 'cc':
+            cadastrar_conta(cc['usuario'])
+        elif opcao == 'q':
+            break
+        else:
+            print('Operação inválida, por favor selevione novamente a operação desejada.')
+
+
+def show_menu_non_cc(num_cpf):
+    menu = '''
+
+    [c] Cadastrar usuário
+    [cc] Cadastrar conta
+    [q] Sair
+
+    => '''
+
+    while True:
+        opcao = input(menu)
+
+        if opcao == 'c':
+            cadastrar_usuario()
+        elif opcao == 'cc':
+            cadastrar_conta(num_cpf)
+        elif opcao == 'q':
+            break
+
+
+def obter_dados_cc(num_cpf, num_cc):
+    try:
+        with open('contas.csv', 'r') as file:
+            reader = csv.reader(file)
+            for linha in reader:
+                if linha[0] == num_cc and linha[2] == num_cpf:
+                    # print(f'Conta: {linha[0]}, Agência: {linha[1]}, Usuário: {linha[2]}, Saldo: {linha[3]}')
+                    cc = {
+                        'numero_conta': linha[0],
+                        'agencia': linha[1],
+                        'usuario': linha[2],
+                        'saldo': float(linha[3]),
+                        'extrato': linha[4]
+                    }
+                    return cc
+    except FileNotFoundError:
+        print('Arquivo de contas não encontrado.')
+
+
+def atualizar_conta(num_cc, agencia, usuario, saldo, extrato):
+    try:
+        with open('contas.csv', 'r') as file:
+            reader = list(csv.reader(file))
+        
+        with open('contas.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            for linha in reader:
+                if linha[0] == num_cc and linha[2] == usuario:
+                    linha[3] = str(saldo)
+                    linha[4] = extrato
+                writer.writerow(linha)
+    except FileNotFoundError:
+        print('Arquivo de contas não encontrado.')
+
+
+def main():
+    print('Bem-vindo ao sistema bancário!')
+    num_cpf = input('Digite seu cpf: ')
+    num_cc = input('Digite sua conta: ')
+    
+    try:
+        cc = obter_dados_cc(num_cpf, num_cc)
+        show_menu_cc(cc)
+    except Exception as e:
+        print(f'Erro ao obter dados da conta')
+        show_menu_non_cc(num_cpf)
+
+
+main()
--- operacoes.py
+++ operacoes.py
@@ -1,3 +1,7 @@
+from Usuario import Usuario
+from Conta import Conta
+
+
 def deposito(saldo, extrato):
     while True:
         valor_deposito = float(input('Informe o valor do deposito: '))
@@ -6,7 +10,7 @@ def deposito(saldo, extrato):
             print('Valor inválido. Favor tente novamente.')
         else: 
             saldo += valor_deposito
-            extrato += f'Depósito: R$ {valor_deposito:.2f}\n'
+            extrato += f'Depósito: R$ {valor_deposito:.2f}\\n'
             return saldo, extrato
 
 
@@ -24,7 +28,7 @@ def saque(saldo, extrato, numero_saques, limite, LIMITE_SAQUES):
             print('Valor inválido.')
         else:
             saldo -= valor_saque
-            extrato += f'Saque: R$ {valor_saque:.2f}\n'
+            extrato += f'Saque: R$ {valor_saque:.2f}\\n'
             numero_saques += 1
     
     return saldo, extrato, numero_saques
@@ -32,4 +36,32 @@ def saque(saldo, extrato, numero_saques, limite, LIMITE_SAQUES):
 
 def mostrar_extrato(extrato, saldo):
     print('--- Extrato ---')
-    print(extrato + f'Saldo: R$ {saldo:.2f}')
+    extrato_formatado = extrato.replace('\\n', '\n')
+    print(extrato_formatado + f'Saldo: R$ {saldo:.2f}')
+
+
+def cadastrar_usuario():
+    nome = input('Informe o nome: ')
+    data_nascimento = input('Informe a data de nascimento: ')
+    cpf = input('Informe o CPF: ')
+    logradouro = input('Informe o logradouro: ')
+    numero = input('Informe o número: ')
+    bairro = input('Informe o bairro: ')
+    cidade = input('Informe a cidade: ')
+    uf = input('Informe a sigla do estado: ')
+
+    usuario = Usuario(nome, data_nascimento, cpf, logradouro, numero, bairro, cidade, uf)
+    print('Usuário cadastrado com sucesso!')
+
+    return usuario.cpf
+
+
+def cadastrar_conta(num_cpf):
+    if num_cpf == None:
+        print('Você precisa cadastrar um usuário antes de criar uma conta.')
+        num_cpf = cadastrar_usuario()
+
+    conta = Conta(num_cpf)
+    print('Conta cadastrada com sucesso!')
+
+    return conta.numero_conta
